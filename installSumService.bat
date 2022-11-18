6d503c472a6d582761a8b334f59b8b52be72a406
Artyom <artem.lepesha@mail.ru>
2022-11-18 11:52:35 +0300

    microchanges

--- installSumService.bat
+++ installSumService.bat
@@ -1,3 +1,4 @@
 call gradle build
-call copy /-y build\libs\service_sum-0.0.1-SNAPSHOT.jar
+call copy build\libs\service_sum-0.0.1-SNAPSHOT.jar
+call del "sum_service.jar"
 call rename "service_sum-0.0.1-SNAPSHOT.jar" "sum_service.jar"
